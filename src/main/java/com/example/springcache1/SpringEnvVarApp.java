b74ea743a5e33ea6e6879189661f8deb70808c60
Maksym Vasiutin <max0l0rose@gmail.com>
2023-01-10 17:08:19 +0100

    Init 2

---
+++ src/main/java/com/example/springcache1/SpringEnvVarApp.java
@@ -0,0 +1,11 @@
+package com.example.springcache1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class SpringEnvVarApp {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEnvVarApp.class, args);
+	}
+}
