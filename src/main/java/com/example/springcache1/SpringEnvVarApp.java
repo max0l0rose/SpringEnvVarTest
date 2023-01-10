e640107f3a64df013ae574fc2ec13e1ec6a1b75d
Maksym Vasiutin <max0l0rose@gmail.com>
2023-01-10 17:26:30 +0100

    1111

--- src/main/java/com/example/springcache1/SpringEnvVarApp.java
+++ src/main/java/com/example/springcache1/SpringEnvVarApp.java
@@ -1,10 +1,22 @@
 package com.example.springcache1;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication()
 public class SpringEnvVarApp {
+
+	@Value("${var1}")
+	String var1;
+
+	@Bean
+	boolean bean1() {
+		System.out.println("var1 = " + var1);
+		return true;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEnvVarApp.class, args);
 	}
